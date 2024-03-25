2d769082971e98938ab49b1438ebc32cce21212f
pdrVenancio <pedrosantos121229@gmail.com>
2024-03-25 12:53:21 -0300

    tuplas e dicionarios

--- Opedracoes_basicas/_.py
+++ Opedracoes_basicas/_.py
@@ -152,3 +152,95 @@ arvore.inserir(3)
 
 print(arvore.buscar(5))  # Saída: True
 print(arvore.buscar(20)) # Saída: False
+
+######################################################################
+# Dicionario
+
+# Criar um dicionário
+my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+
+# Acessar um valor através da chave
+value = my_dict['key1']
+
+# Adicionar um novo par chave-valor
+my_dict['new_key'] = 'new_value'
+
+# Remover um par chave-valor
+del my_dict['key1']
+
+# Verificar se uma chave está presente
+if 'key1' in my_dict:
+    print("Key exists!")
+
+# Iterar sobre chaves e valores
+for key, value in my_dict.items():
+    print(key, value)
+
+# Obter todas as chaves ou valores
+keys = my_dict.keys()
+values = my_dict.values()
+
+# Obter o número de pares chave-valor
+num_items = len(my_dict)
+
+# Copiar um dicionário
+new_dict = my_dict.copy()
+
+# Limpar todos os itens de um dicionário
+my_dict.clear()
+
+# Combinar dois dicionários
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+
+# Quando você usa ** com dicionários, ele 
+#  desempacota os elementos do dicionário 
+#  fornecido e os adiciona ao novo dicionário.
+combined_dict = {**dict1, **dict2}
+
+# Obter um valor padrão se a chave não existir
+value = my_dict.get('key', 'default_value')
+
+#####################################################################
+# Tuplas
+
+# Criando uma tupla
+tupla = (1, 2, 3, 4, 5)
+
+# Acessando elementos
+primeiro_elemento = tupla[0]
+ultimo_elemento = tupla[-1]
+
+# Fatiamento de tuplas
+sub_tupla = tupla[1:3]
+
+# Concatenação de tuplas
+nova_tupla = tupla + (6, 7, 8)
+
+# Verificando se um elemento está presente
+if 5 in tupla:
+    print("5 está na tupla")
+
+# Tamanho da tupla
+tamanho = len(tupla)
+
+# Contagem de elementos
+count = tupla.count(3)
+
+# Encontrando o índice de um elemento
+indice = tupla.index(4)
+
+# Desempacotamento de tuplas
+a, b, c, d, e = tupla
+
+# Criando tuplas vazias
+tupla_vazia = ()
+
+# Convertendo outros tipos para tuplas
+lista = [1, 2, 3]
+tupla_da_lista = tuple(lista)
+
+# Iterando sobre uma tupla
+for item in tupla:
+    print(item)
+
